7d6cd06a4ccd375258a07980ad92945b61a0972d
Otavio L. de Giacometti <otaviolg2000@gmail.com>
2024-03-05 18:09:30 -0300

    Order, Instant, ISO 8601 - part 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity // usado no JPA pra marcar uma entidade
@@ -22,6 +25,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client") // na classe em questão, o atributo é mapeado pelo client
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -90,9 +96,9 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
+
+	public List<Order> getOrders() {
+		return orders;
+	}
 	
 }
