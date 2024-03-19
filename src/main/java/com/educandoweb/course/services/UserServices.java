fb868918493026c5c6bd9f09d157b38e228f9d14
Otavio L. de Giacometti <otaviolg2000@gmail.com>
2024-03-19 20:11:54 -0300

    Exception - update

--- src/main/java/com/educandoweb/course/services/UserServices.java
+++ src/main/java/com/educandoweb/course/services/UserServices.java
@@ -1,6 +1,5 @@
 package com.educandoweb.course.services;
 
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +12,8 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserServices {
@@ -46,10 +47,15 @@ public class UserServices {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
